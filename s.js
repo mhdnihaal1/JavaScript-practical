f2a88097cbb281fdab311a4f49da2bf7116320da
mhdnihaal1 <nihalmuhaednihal@gmail.com>
2024-07-09 04:58:44 -0700

    first commit

---
+++ s.js
@@ -0,0 +1,58 @@
+
+// const timeoutId = setTimeout(() => {
+//     console.log('Timeout completed after 10 seconds');
+// }, 10000);
+
+// setTimeout(() => {
+//     clearTimeout(timeoutId);
+//     console.log('Timeout cleared after 2 seconds');
+// }, 2000);
+
+//============================================
+
+// const set = setInterval(()=>{
+//     console.log(123);
+// },3000)
+
+// setTimeout(()=>{
+//     clearInterval(set)
+//     console.log('ok');
+// },10000)
+//====================count====================
+
+// cars = ["Toyota", "Honda", "Ford", "Toyota", "Chevrolet", "Honda", "Tesla", "Ford", "Tesla",'Toyota']
+
+//     const reduce = cars.reduce((fir,sec)=> (sec==='Toyota'?fir+1:fir),0)
+
+// console.log(reduce);
+//======================================================
+
+
+// const arr = [5,2,2,1,7,5,5]
+// let arrr=[]
+//     for(let i=0;i<arr.length;i++){
+//         let one=true
+//         for(let j=0;j<arr.length;j++){
+//             if(j!==i && arr[i]==arr[j]){
+//              one=false
+//             }
+//         }
+//         if(one){
+//             arrr.push(arr[i])
+
+//         }
+//     }
+
+
+// console.log(arrr);
+
+
+
+// const timeoutId = setTimeout(()=>{
+//     console.log("Delayed function executed after 2000 milliseconds");
+// },5000)
+
+// setTimeout(()=>{clearTimeout(timeoutId)
+//     console.log('k');},3000)
+
+// clearInterval(timeoutId,5000);
---
+++ sample2.js
@@ -0,0 +1,3 @@
+const string ="Bob hit a ball, the hit BALL flew far after it was hit."
+const s=string.split(' ')
+console.log(s)
